ac8e177d64918db62fbe70d7d43710c1efa3d173
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-06-23 11:33:19 +0300

    create restaurant functionality with react hook form and zod for validation

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,5 +1,5 @@
 import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
-import UserProfileForm from "@/form/user-profile-form/UserProfileForm";
+import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
   const { currentUser, isLoading: isGetLoading } = useGetMyUser();
