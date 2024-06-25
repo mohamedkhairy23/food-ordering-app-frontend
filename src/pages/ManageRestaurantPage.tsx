a4a87ec9663de2c61628dd601bd6461af9af1310
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-06-25 11:32:18 +0300

    little change

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -3,17 +3,22 @@ import {
   useGetMyRestaurant,
   useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
+import Spinner from "@/components/Spinner";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
   const { createRestaurant, isLoading: isLoadingCreate } =
     useCreateMyRestaurant();
-  const { restaurant } = useGetMyRestaurant();
+  const { restaurant, isLoading } = useGetMyRestaurant();
   const { updateRestaurant, isLoading: isLoadingUpdate } =
     useUpdateMyRestaurant();
 
   // !!restaurant synatax mean give me truthy value if a restaurant exists or not
   const isEditting = !!restaurant;
+
+  if (isLoading) {
+    return <Spinner isLoading={isLoading} />;
+  }
 
   return (
     <ManageRestaurantForm
