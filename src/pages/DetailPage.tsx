d679a55de85af4073ba0dd9a3da65eefe91eca88
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2024-06-26 01:27:43 +0300

    handling creating checkout session without creating order

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -10,6 +10,7 @@ import { useParams } from "react-router-dom";
 import { MenuItem as MenuItemType } from "../types";
 import CheckoutButton from "@/components/CheckoutButton";
 import { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
+import { useCreateCheckoutSession } from "@/api/OrderApi";
 
 export type CartItem = {
   _id: string;
@@ -20,7 +21,10 @@ export type CartItem = {
 
 const DetailPage = () => {
   const { restaurantId } = useParams();
-  const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+  const { restaurant, isLoading: isLoadingGetRestaurant } =
+    useGetRestaurant(restaurantId);
+  const { createCheckoutSession, isLoading: isLoadingCreateCheckoutSession } =
+    useCreateCheckoutSession();
 
   const [cartItems, setCartItems] = useState<CartItem[]>(() => {
     const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
@@ -73,12 +77,34 @@ const DetailPage = () => {
     });
   };
 
-  const onCheckout = (userFormData: UserFormData) => {
+  const onCheckout = async (userFormData: UserFormData) => {
     console.log("userFormData", userFormData);
+
+    if (!restaurant) {
+      return;
+    }
+
+    const checkoutData = {
+      cartItems: cartItems.map((cartItem) => ({
+        menuItemId: cartItem._id,
+        name: cartItem.name,
+        quantity: cartItem.quantity.toString(),
+      })),
+      restaurantId: restaurant._id,
+      deliveryDetails: {
+        name: userFormData.name,
+        addressLine1: userFormData.addressLine1,
+        city: userFormData.city,
+        email: userFormData.email as string,
+      },
+    };
+
+    const data = await createCheckoutSession(checkoutData);
+    window.location.href = data.url;
   };
 
-  if (isLoading || !restaurant) {
-    return <Spinner isLoading={isLoading} />;
+  if (isLoadingGetRestaurant || !restaurant) {
+    return <Spinner isLoading={isLoadingGetRestaurant} />;
   }
 
   return (
@@ -113,7 +139,7 @@ const DetailPage = () => {
               <CheckoutButton
                 disabled={cartItems.length === 0}
                 onCheckout={onCheckout}
-                isLoading={false}
+                isLoading={isLoadingCreateCheckoutSession}
               />
             </CardFooter>
           </Card>
